01ddac9a37b003a147f7cc021770c5f74faf759f
Andres Loaiza <andres.loaiza@outlook.it>
2021-11-24 12:19:49 +0100

    added icon,commit status and settings

---
+++ T3UploaderWPF/Network/Data/GitCommitStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3UploaderWPF.Network.Data
+{
+    public class GitCommitStatus
+    {
+        public bool Dirty { get; set; }
+        public string Message { get; set; }
+    }
+}
